83e49a9d1e3396dcf155ca7a2fdf8c7a6c68858b
Yao Hao <yhaoao@163.com>
2014-12-14 04:34:29 +0800

    add screen

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ ngTab
 =====
 
 仿微信主界面
+![1](https://github.com/yhaoao/ngTab/blob/master/screen/1.git)
--- js/app.js
+++ js/app.js
@@ -1,199 +1,229 @@
-angular.module('weixin', ['ngTouch']).controller('MainCtrl', function() {
-		var self = this;
-		self.startedTime = new Date().getTime();
-		self.stocks = [{
-			name: 'First Stock',
-			price: 100,
-			previous: 220
-		}, {
-			name: 'Second Stock',
-			price: 140,
-			previous: 120
-		}, {
-			name: 'Third Stock',
-			price: 110,
-			previous: 110
-		}, {
-			name: 'Fourth Stock',
-			price: 400,
-			previous: 420
-		}];
+angular.module('weixin', ['ngTouch']).controller('MainCtrl', function(Environment) {
 
-		self.point = {
-			x: 400,
-			y: 400
-		}
+		Environment.init();
+    })
+    .factory('Environment', function($window) {
 
-	})
-	.factory('Environment', function($window) {
-		var winDimision = {
-			width: $window.innerWidth
-		};
-		return {
-			winDimision: winDimision
-		}
-	})
-	.directive('tabs', ['Environment', '$swipe', function(Environment, $swipe) {
-		return {
-			restrict: 'E',
-			transclude: true,
-			scope: true,
-			template: '<div class="tab-headers" id="tab-headers">' +
-				'  <div ng-repeat="tab in tabs"' +
-				'       class="tab-item"' +
-				'       ng-click="selectTab($index)"' +
-				'       id="{{\'tab\'+$index}}"' +
-				'       ng-class="{selected: isSelectedTab($index)}">' +
-				'	  <i style="font-size:2.5em;line-height:1em" ng-class="tab.icon"></i></br>' +
-				'     <span ng-bind="tab.title"></span>' +
-				'  </div>' +
-				'</div>' +
-				'<div class="tab-container" id="tab-container" ng-transclude></div> ',
-			controller: function($scope) {
-				var currentIndex = 0;
-				$scope.tabs = [];
+        var init = function() {
+            this.winDimision = {};
+            this.winDimision.width = $window.innerWidth
+        }
+        return {
 
-				$scope.selectTab = function(index, transition) {
-					currentIndex = index;
-					var tabs = document.querySelector('#tab-headers');
-					var tabTransition = document.querySelector('#tabTransition');
+            init: init
+        }
+    })
+    .factory('Bezier', function() {
+        var bezier = function(x1, y1, x2, y2, epsilon) {
 
-					tabTransition.disabled = !transition;
+            var curveX = function(t) {
+                var v = 1 - t;
+                return 3 * v * v * t * x1 + 3 * v * t * t * x2 + t * t * t;
+            };
+            var curveY = function(t) {
+                var v = 1 - t;
+                return 3 * v * v * t * y1 + 3 * v * t * t * y2 + t * t * t;
+            };
+            var derivativeCurveX = function(t) {
+                var v = 1 - t;
+                return 3 * (2 * (t - 1) * t + v * v) * x1 + 3 * (-t * t * t + 2 * v * t) * x2;
+            };
+            return function(t) {
 
-					for (var i = 0; i < $scope.tabs.length; i++) {
-						if (i === currentIndex) {
-							$scope.tabs[i].scope.isShowed = true;
-							$scope.tabs[i].scope.style["-webkit-transform"] = 'translate3d(0, 0, 0)';
-							$scope.tabs[i].scope.style["opacity"] = 1;
-						} else if ((i - 1) === currentIndex) {
-							$scope.tabs[i].scope.isShowed = true;
-							$scope.tabs[i].scope.style["-webkit-transform"] = 'translate3d(' + Environment.winDimision.width + 'px, 0, 0)';
-							$scope.tabs[i].scope.style["opacity"] = 0;
+                var x = t,
+                    t0, t1, t2, x2, d2, i;
 
+                for (t2 = x, i = 0; i < 8; i++) {
+                    x2 = curveX(t2) - x;
+                    if (Math.abs(x2) < epsilon) return curveY(t2);
+                    d2 = derivativeCurveX(t2);
+                    if (Math.abs(d2) < 1e-6) break;
+                    t2 = t2 - x2 / d2;
+                }
 
-						} else if ((i + 1) === currentIndex) {
-							$scope.tabs[i].scope.isShowed = true;
-							$scope.tabs[i].scope.style["-webkit-transform"] = 'translate3d(' + (-Environment.winDimision.width) + 'px, 0, 0)';
-							$scope.tabs[i].scope.style["opacity"] = 0;
+                t0 = 0, t1 = 1, t2 = x;
 
-						} else {
-							$scope.tabs[i].scope.isShowed = false;
-						}
-					}
-				};
-				$scope.isSelectedTab = function(index) {
-					return currentIndex === index;
-				};
+                if (t2 < t0) return curveY(t0);
+                if (t2 > t1) return curveY(t1);
 
-				this.registerTab = function(title, icon, scope, element) {
-					$scope.tabs.push({
-						title: title,
-						icon: icon,
-						scope: scope
-					});
+                while (t0 < t1) {
+                    x2 = curveX(t2);
+                    if (Math.abs(x2 - x) < epsilon) return curveY(t2);
+                    if (x > x2) t0 = t2;
+                    else t1 = t2;
+                    t2 = (t1 - t0) * .5 + t0;
+                }
 
-					$scope.selectTab(0);
-					var startX, endX, currentTab, leftTab, rightTab;
+                return curveY(t2);
 
-					$swipe.bind(element, {
-						'start': function(coords, event) {
-							currentTab = document.querySelector('#tab' + currentIndex);
-							leftTab = document.querySelector('#tab' + (currentIndex - 1));
-							rightTab = document.querySelector('#tab' + (currentIndex + 1));
+            };
 
-							startX = coords.x;
-						},
-						'move': function(coords, event) {
-							var xLength = coords.x - startX;
-							var tabLen = $scope.tabs.length;
-							if ((currentIndex === 0 && xLength > 0) || (currentIndex === (tabLen - 1) && xLength < 0)) {
-								return;
-							}
+        };;
 
-							var radiao = Math.abs(xLength / Environment.winDimision.width);
+        return {
+            bezier: bezier
+        }
+    })
+    .directive('tabs', ['Environment', '$swipe', 'Bezier', function(Environment, $swipe, Bezier) {
+        return {
+            restrict: 'E',
+            transclude: true,
+            scope: true,
+            templateUrl: 'template/tab_header.html',
+            controller: function($scope) {
+                var currentIndex = 0;
+                $scope.tabs = [];
 
-							var red = Math.floor(64 + (180 - 64) * radiao);
-							var blue = Math.floor(169 + (180 - 169) * radiao);
-							var green = Math.floor(17 + (180 - 17) * radiao);
+                $scope.selectTab = function(index, transition) {
+                    currentIndex = index;
+                    var tabs = document.querySelector('#tab-headers');
+                    var tabTransition = document.querySelector('#tabTransition');
 
-							var nextRed=Math.floor(180- (180 - 64) * radiao);
-							var nextBlue=Math.floor(180- (180 - 169) * radiao);
-							var nextGreen=Math.floor(180- (180 - 17) * radiao);
+                    tabTransition.disabled = !transition;
 
-							var nextTab=leftTab;
-							if(xLength<0){
-								nextTab=rightTab;
-							}
-
-							$scope.$apply(function() {
-								currentTab.style.color = 'rgba(' + red + ',' + blue + ',' + green + ',1)';
-								nextTab.style.color='rgba(' + nextRed + ',' + nextBlue + ',' + nextGreen + ',1)';
-								for (var i = 0; i < tabLen; i++) {
-									if ((i + 1) === currentIndex) {
-										$scope.tabs[i].scope.style["opacity"] = Math.abs(xLength / Environment.winDimision.width);
-										$scope.tabs[i].scope.style["-webkit-transform"] = 'translate3d(' + (-Environment.winDimision.width + xLength) + 'px, 0, 0)';
-									} else if (i === currentIndex) {
-										$scope.tabs[i].scope.style["opacity"] = 1 - Math.abs(xLength / Environment.winDimision.width);
-										$scope.tabs[i].scope.style["-webkit-transform"] = 'translate3d(' + xLength + 'px, 0, 0)';
-									} else if ((i - 1) === currentIndex) {
-										$scope.tabs[i].scope.style["opacity"] = Math.abs(xLength / Environment.winDimision.width);
-										$scope.tabs[i].scope.style["-webkit-transform"] = 'translate3d(' + (Environment.winDimision.width + xLength) + 'px, 0, 0)';
-									}
-
-								}
-							});
-						},
-						'end': function(coords, event) {
-							endX = coords.x;
-							var distence = endX - startX;
-							var tabLen = $scope.tabs.length;
-							currentTab.style.color = '';
-							$scope.$apply(function() {
-								if ((currentIndex === 0 && distence > 0) || (currentIndex === (tabLen - 1) && distence < 0)) {
-									$scope.selectTab(currentIndex, true);
-									return;
-								}
-								if (Math.abs(distence) * 3 > Environment.winDimision.width) {
-									if (distence > 0) {
-										$scope.selectTab(currentIndex - 1, true);
-									} else {
-										$scope.selectTab(currentIndex + 1, true);
-									}
-								} else {
-									$scope.selectTab(currentIndex, true);
-								}
-							});
+                    for (var i = 0; i < $scope.tabs.length; i++) {
+                        if (i === currentIndex) {
+                            $scope.tabs[i].scope.isShowed = true;
+                            $scope.tabs[i].scope.style["-webkit-transform"] = 'translate3d(0, 0, 0)';
+                            $scope.tabs[i].scope.style["opacity"] = 1;
+                        } else if ((i - 1) === currentIndex) {
+                            $scope.tabs[i].scope.isShowed = true;
+                            $scope.tabs[i].scope.style["-webkit-transform"] = 'translate3d(' + Environment.winDimision.width + 'px, 0, 0)';
+                            $scope.tabs[i].scope.style["opacity"] = 0;
 
 
-						},
-						'cancle': function() {
-							$scope.selectTab(currentIndex, true);
-						}
-					}, ['touch']);
+                        } else if ((i + 1) === currentIndex) {
+                            $scope.tabs[i].scope.isShowed = true;
+                            $scope.tabs[i].scope.style["-webkit-transform"] = 'translate3d(' + (-Environment.winDimision.width) + 'px, 0, 0)';
+                            $scope.tabs[i].scope.style["opacity"] = 0;
 
-				};
+                        } else {
+                            $scope.tabs[i].scope.isShowed = false;
+                        }
+                    }
+                };
+                $scope.isSelectedTab = function(index) {
+                    return currentIndex === index;
+                };
+
+                this.registerTab = function(title, icon, scope, element) {
+                    $scope.tabs.push({
+                        title: title,
+                        icon: icon,
+                        scope: scope
+                    });
+
+                    $scope.selectTab(0);
+                    var startX, endX, currentTab, leftTab, rightTab, nextTab;
+                    var easyOut = Bezier.bezier(0, 1, 1, 1, (1000 / 60 / 500) / 4);
+                    $swipe.bind(element, {
+                        'start': function(coords, event) {
+                            currentTab = document.querySelector('#tab' + currentIndex);
+                            leftTab = document.querySelector('#tab' + (currentIndex - 1));
+                            rightTab = document.querySelector('#tab' + (currentIndex + 1));
+
+                            startX = coords.x;
+                        },
+                        'move': function(coords, event) {
+                            var xLength = coords.x - startX;
+                            var tabLen = $scope.tabs.length;
+                            if ((currentIndex === 0 && xLength > 0) || (currentIndex === (tabLen - 1) && xLength < 0)) {
+                                return;
+                            }
+
+                            var radiao = Math.abs(xLength / Environment.winDimision.width);
+
+                            
+
+                            var red = Math.floor(64 + (180 - 64) * easyOut(radiao));
+                            var blue = Math.floor(169 + (180 - 169) * easyOut(radiao));
+                            var green = Math.floor(17 + (180 - 17) * easyOut(radiao));
+
+                            var nextRed = Math.floor(180 - (180 - 64) * easyOut(radiao));
+                            var nextBlue = Math.floor(180 - (180 - 169) * easyOut(radiao));
+                            var nextGreen = Math.floor(180 - (180 - 17) * easyOut(radiao));
+
+                            nextTab = leftTab;
+                            if (xLength < 0) {
+                                nextTab = rightTab;
+                            }
+
+                            $scope.$apply(function() {
+                                currentTab.style.color = 'rgba(' + red + ',' + blue + ',' + green + ',1)';
+                                nextTab.style.color = 'rgba(' + nextRed + ',' + nextBlue + ',' + nextGreen + ',1)';
+                                for (var i = 0; i < tabLen; i++) {
+                                    if ((i + 1) === currentIndex) {
+                                        $scope.tabs[i].scope.style['opacity'] = Math.abs(xLength / Environment.winDimision.width);
+                                        $scope.tabs[i].scope.style["-webkit-transform"] = 'translate3d(' + (-Environment.winDimision.width + xLength) + 'px, 0, 0)';
+                                    } else if (i === currentIndex) {
+                                        $scope.tabs[i].scope.style['opacity'] = 1 - Math.abs(xLength / Environment.winDimision.width);
+                                        $scope.tabs[i].scope.style["-webkit-transform"] = 'translate3d(' + xLength + 'px, 0, 0)';
+                                    } else if ((i - 1) === currentIndex) {
+                                        $scope.tabs[i].scope.style["opacity"] = Math.abs(xLength / Environment.winDimision.width);
+                                        $scope.tabs[i].scope.style["-webkit-transform"] = 'translate3d(' + (Environment.winDimision.width + xLength) + 'px, 0, 0)';
+                                    }
+
+                                }
+                            });
+                        },
+                        'end': function(coords, event) {
+                            endX = coords.x;
+                            var distence = endX - startX;
+                            var tabLen = $scope.tabs.length;
+                            currentTab.style.color = '';
+                            if(nextTab){
+                            	nextTab.style.color = '';
+                            }
+                            
+                            $scope.$apply(function() {
+                                if ((currentIndex === 0 && distence > 0) || (currentIndex === (tabLen - 1) && distence < 0)) {
+
+                                    $scope.selectTab(currentIndex, true);
+                                    return;
+                                }
+                                if (Math.abs(distence) * 3 > Environment.winDimision.width) {
+                                    if (distence > 0) {
+                                        $scope.selectTab(currentIndex - 1, true);
+                                    } else {
+                                        $scope.selectTab(currentIndex + 1, true);
+                                    }
+                                } else {
+                                    $scope.selectTab(currentIndex, true);
+                                }
+                            });
+
+
+                        },
+                        'cancel': function() {
+                        	currentTab.style.color = '';
+                            nextTab.style.color = '';
+                            $scope.selectTab(currentIndex, true);
+                        }
+                    }, ['touch']);
+
+                };
 
 
 
-			}
-		};
-	}])
-	.directive('tab', ['Environment', function(Environment) {
-		return {
-			restrict: 'E',
-			transclude: true,
-			template: '<div class="tab-content"  ng-show="isShowed" ng-style="style" ng-transclude></div>',
-			require: '^tabs',
-			scope: true,
-			link: function($scope, $element, $attr, tabCtrl) {
-				$scope.style = {
-					width: Environment.winDimision.width
-				};
-				var tabIndex = tabCtrl.registerTab($attr.title, $attr.icon, $scope, $element);
+            }
+        };
+    }])
+    .directive('tab', ['Environment', function(Environment) {
+        return {
+            restrict: 'E',
+            transclude: true,
+            template: '<div class="tab-content"  ng-show="isShowed" ng-style="style" ng-transclude></div>',
+            require: '^tabs',
+            scope: true,
+            link: function($scope, $element, $attr, tabCtrl) {
+                $scope.style = {
+                    width: Environment.winDimision.width
+                };
+                var tabIndex = tabCtrl.registerTab($attr.title, $attr.icon, $scope, $element);
 
-			}
-		};
-	}])
-	.run(function() {
-
-	});
+            }
+        };
+    }])
+    .run(function(Environment) {
+        
+    });
